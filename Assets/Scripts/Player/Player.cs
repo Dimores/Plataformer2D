fe6c83cfb02662de50241d2cd2a4d6ca4a2d0b14
Diego Morelo Alvares Duar <xxkirito31@gmail.com>
2025-01-31 18:15:06 -0300

    Adding run key

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigdbody;
-
     public float speed;
-
     public float jumpForce = 5f;
 
+    private float _speedRun;
+    private float _currentSpeed;
     private Vector2 _friction = new Vector2(-.1f, 0);
+
+    private void Start()
+    {
+        // 50% More faster than walking
+        _speedRun = speed + (speed * 0.5f);
+    }
 
     void Update()
     {
@@ -20,13 +26,18 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
+        if (Input.GetKey(KeyCode.LeftControl))
+            _currentSpeed = _speedRun;
+        else
+            _currentSpeed = speed;
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            myRigdbody.velocity = new Vector2(-speed, myRigdbody.velocity.y);
+            myRigdbody.velocity = new Vector2(-_currentSpeed, myRigdbody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            myRigdbody.velocity = new Vector2(speed, myRigdbody.velocity.y);
+            myRigdbody.velocity = new Vector2(_currentSpeed, myRigdbody.velocity.y);
         }
 
         if (myRigdbody.velocity.x > 0)
