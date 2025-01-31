e772472020bda3601f4bf95a19fe8c5a4ad60203
Diego Morelo Alvares Duar <xxkirito31@gmail.com>
2025-01-31 18:03:00 -0300

    Implement first version of jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,18 +6,42 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigdbody;
 
-    public Vector2 velocity;
-
     public float speed;
 
+    public float jumpForce = 5f;
+
+    private Vector2 _friction = new Vector2(-.1f, 0);
+
     void Update()
+    {
+        HandleJump();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             myRigdbody.velocity = new Vector2(-speed, myRigdbody.velocity.y);
-        } else if (Input.GetKey(KeyCode.RightArrow))
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             myRigdbody.velocity = new Vector2(speed, myRigdbody.velocity.y);
         }
+
+        if (myRigdbody.velocity.x > 0)
+        {
+            myRigdbody.velocity += _friction;
+        } else if (myRigdbody.velocity.x < 0)
+        {
+            myRigdbody.velocity -= _friction;
+        }
     }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            myRigdbody.velocity = Vector2.up * jumpForce;
+    }
+
 }
