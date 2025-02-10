de16deedba4ab8b9d39dd3e4ac512e4cd8112559
Diego Morelo Alvares Duar <xxkirito31@gmail.com>
2025-02-10 16:14:25 -0300

    Add coin value to UI and second enemy

--- Assets/Scripts/Item/ItemCollectableCoin.cs
+++ Assets/Scripts/Item/ItemCollectableCoin.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening; // Importa o DOTween
 
 public class ItemCollectableCoin : ItemCollectableBase
 {
+    [Header("Configurações da Animação")]
+    public float rotationSpeed = 1f; 
+    public float floatHeight = 0.2f; 
+    public float floatDuration = 1f; 
+
+    private Tween _rotationTween;
+    private Tween _floatTween;
+
+    private void Start()
+    {
+        AnimateCoin(); 
+    }
+
+    private void AnimateCoin()
+    {
+        _rotationTween = transform.DORotate(new Vector3(0, 360, 0), rotationSpeed, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Restart)
+            .SetEase(Ease.Linear); 
+
+        _floatTween = transform.DOMoveY(transform.position.y + floatHeight, floatDuration)
+            .SetLoops(-1, LoopType.Yoyo) 
+            .SetEase(Ease.InOutSine); 
+    }
+
     protected override void OnCollect()
     {
         ItemManager.Instance.AddCoins();
+
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        _rotationTween?.Kill();
+        _floatTween?.Kill();
     }
 }
--- Assets/Scripts/Item/ItemManager.cs
+++ Assets/Scripts/Item/ItemManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ebac.Core.Singleton;
+using TMPro;
 
 public class ItemManager : Singleton<ItemManager>
 {
     public int coins;
+    public TextMeshProUGUI coinText;
 
     private void Start()
     {
@@ -15,10 +17,17 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins = 0;
+        UpdateCoinUI();
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateCoinUI();
+    }
+
+    private void UpdateCoinUI()
+    {
+        coinText.text = "x " + coins.ToString();
     }
 }
