76e8badb49ac2668a5a212c042c5575ba664cc13
Diego Morelo Alvares Duar <xxkirito31@gmail.com>
2025-02-10 07:53:22 -0300

    Update item manager

--- Assets/Scripts/Item/ItemCollectableBase.cs
+++ Assets/Scripts/Item/ItemCollectableBase.cs
@@ -15,7 +15,6 @@ public class ItemCollectableBase : MonoBehaviour
     }
 
     protected virtual void Collect() {
-        Debug.Log("Collect");
         gameObject.SetActive(false);
         OnCollect();
     }
